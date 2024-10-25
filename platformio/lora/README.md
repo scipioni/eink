fa8da0ecff09af76192518f0e01c80cc3c015044
Stefano Scipioni <scipio.it@gmail.com>
2024-10-25 19:35:02 +0200

    feat: web server wifi

--- platformio/lora/README.md
+++ platformio/lora/README.md
@@ -3,4 +3,5 @@
 - https://github.com/HelTecAutomation/Heltec_ESP32
 - https://github.com/ropg/Heltec_ESP32_LoRa_v3
 - https://github.com/HelTecAutomation/Heltec_ESP32/blob/master/examples/LoRaBasic/pingpong/pingpong.ino
+- https://microcontrollerslab.com/esp32-rest-api-web-server-get-post-postman/
  
--- platformio/lora/src/main.cpp
+++ platformio/lora/src/main.cpp
@@ -2,6 +2,11 @@
 //  Pause between sends in seconds, so this is every 15 minutes. (Delay will be
 //  longer if regulatory or TTN Fair Use Policy requires it.)
 
+#include <ArduinoJson.h>
+#include <ESPmDNS.h>
+#include <WebServer.h>
+#include <WiFi.h>
+
 #define HELTEC_WIRELESS_STICK_LITE
 // Turns the 'PRG' button into the power button, long press is off
 #define HELTEC_POWER_BUTTON // must be before "#include <heltec_unofficial.h>"
@@ -17,17 +22,15 @@
 // Allowed values are 7.8, 10.4, 15.6, 20.8, 31.25, 41.7, 62.5, 125.0, 250.0 and
 // 500.0 kHz.
 #define BANDWIDTH 250.0
-
 // Number from 5 to 12. Higher means slower but higher "processor gain",
 // meaning (in nutshell) longer range and more robust against interference.
 #define SPREADING_FACTOR 9
 
-// Transmit power in dBm. 0 dBm = 1 mW, enough for tabletop-testing. This value
-// can be set anywhere between -9 dBm (0.125 mW) to 22 dBm (158 mW). Note that
-// the maximum ERP (which is what your antenna maximally radiates) on the EU ISM
-// band is 25 mW, and that transmissting without an antenna can damage your
-// hardware.
-#define TRANSMIT_POWER 2
+// Transmit power in dBm. 0 dBm = 1 mW, enough for tabletop-testing. This
+// value can be set anywhere between -9 dBm (0.125 mW) to 22 dBm (158 mW).
+// Note that the maximum ERP (which is what your antenna maximally radiates)
+// on the EU ISM band is 25 mW, and that transmissting without an antenna
+// can damage your hardware. #define TRANSMIT_POWER 2
 
 String rxdata;
 String txdata;
@@ -37,7 +40,62 @@ uint64_t last_tx = 0;
 uint64_t tx_time;
 uint64_t minimum_pause;
 
-// Can't do Serial or display things here, takes too much time for the interrupt
+WebServer server(80);
+StaticJsonDocument<250> jsonDocument;
+char buffer[250];
+
+void wifi_init() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+}
+
+void create_json(char *tag, float value, char *unit) {
+  jsonDocument.clear();
+  jsonDocument["type"] = tag;
+  jsonDocument["value"] = value;
+  jsonDocument["unit"] = unit;
+  serializeJson(jsonDocument, buffer);
+}
+
+void add_json_object(char *tag, float value, char *unit) {
+  JsonObject obj = jsonDocument.createNestedObject();
+  obj["type"] = tag;
+  obj["value"] = value;
+  obj["unit"] = unit;
+}
+void get_status() {
+  jsonDocument.clear();
+  JsonObject obj = jsonDocument.createNestedObject();
+  obj["status"] = "ok";
+  // add_json_object("temperature", temperature, "°C");
+  serializeJson(jsonDocument, buffer);
+  server.send(200, "application/json", buffer);
+}
+
+void handlePost() {
+  if (server.hasArg("plain") == false) {
+  }
+  String body = server.arg("plain");
+  deserializeJson(jsonDocument, body);
+
+  // action
+  // int red_value = jsonDocument["red"];
+
+  server.send(200, "application/json", "{}");
+}
+void setup_routing() {
+  server.on("/", get_status);
+  server.on("/msg", HTTP_POST, handlePost);
+
+  server.begin();
+}
+
 void rx() { rxFlag = true; }
 
 void setup() {
@@ -58,6 +116,7 @@ void setup() {
   // Start receiving
   RADIOLIB_OR_HALT(radio.startReceive(RADIOLIB_SX126X_RX_TIMEOUT_INF));
   heltec_led(50); // 50% brightness is plenty for this LED
+  wifi_init();
 }
 
 void loop() {
