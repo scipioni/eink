008b370e20516e861daae20e900ba41719fe9d06
Stefano Scipioni <scipio.it@gmail.com>
2024-10-25 07:11:25 +0200

    feat: tx/rx works

--- platformio/lora/lib/heltec_esp32_lora_v3/examples/LoRa_rx_tx/LoRa_rx_tx.ino
+++ platformio/lora/lib/heltec_esp32_lora_v3/examples/LoRa_rx_tx/LoRa_rx_tx.ino
@@ -1,38 +1,42 @@
-// #include <Arduino.h>
-//  Pause between sends in seconds, so this is every 15 minutes. (Delay will be
-//  longer if regulatory or TTN Fair Use Policy requires it.)
+/**
+ * Send and receive LoRa-modulation packets with a sequence number, showing RSSI
+ * and SNR for received packets on the little display.
+ *
+ * Note that while this send and received using LoRa modulation, it does not do
+ * LoRaWAN. For that, see the LoRaWAN_TTN example.
+ *
+ * This works on the stick, but the output on the screen gets cut off.
+*/
 
-#define HELTEC_WIRELESS_STICK_LITE
-// Turns the 'PRG' button into the power button, long press is off
-#define HELTEC_POWER_BUTTON // must be before "#include <heltec_unofficial.h>"
-#define HELTEC_NO_DISPLAY_INSTANCE
+
+
+// Turns the 'PRG' button into the power button, long press is off 
+#define HELTEC_POWER_BUTTON   // must be before "#include <heltec_unofficial.h>"
 #include <heltec_unofficial.h>
 
 // Pause between transmited packets in seconds.
 // Set to zero to only transmit a packet when pressing the user button
 // Will not exceed 1% duty cycle, even if you set a lower value.
-#define PAUSE 300
+#define PAUSE               300
 
 // Frequency in MHz. Keep the decimal point to designate float.
 // Check your own rules and regulations to see what is legal where you are.
-#define FREQUENCY 866.3 // for Europe
+#define FREQUENCY           866.3       // for Europe
 // #define FREQUENCY           905.2       // for US
 
 // LoRa bandwidth. Keep the decimal point to designate float.
-// Allowed values are 7.8, 10.4, 15.6, 20.8, 31.25, 41.7, 62.5, 125.0, 250.0 and
-// 500.0 kHz.
-#define BANDWIDTH 250.0
+// Allowed values are 7.8, 10.4, 15.6, 20.8, 31.25, 41.7, 62.5, 125.0, 250.0 and 500.0 kHz.
+#define BANDWIDTH           250.0
 
 // Number from 5 to 12. Higher means slower but higher "processor gain",
-// meaning (in nutshell) longer range and more robust against interference.
-#define SPREADING_FACTOR 9
+// meaning (in nutshell) longer range and more robust against interference. 
+#define SPREADING_FACTOR    9
 
-// Transmit power in dBm. 0 dBm = 1 mW, enough for tabletop-testing. This value
-// can be set anywhere between -9 dBm (0.125 mW) to 22 dBm (158 mW). Note that
-// the maximum ERP (which is what your antenna maximally radiates) on the EU ISM
-// band is 25 mW, and that transmissting without an antenna can damage your
-// hardware.
-#define TRANSMIT_POWER 2
+// Transmit power in dBm. 0 dBm = 1 mW, enough for tabletop-testing. This value can be
+// set anywhere between -9 dBm (0.125 mW) to 22 dBm (158 mW). Note that the maximum ERP
+// (which is what your antenna maximally radiates) on the EU ISM band is 25 mW, and that
+// transmissting without an antenna can damage your hardware.
+#define TRANSMIT_POWER      0
 
 String rxdata;
 volatile bool rxFlag = false;
@@ -40,9 +44,6 @@ long counter = 0;
 uint64_t last_tx = 0;
 uint64_t tx_time;
 uint64_t minimum_pause;
-
-// Can't do Serial or display things here, takes too much time for the interrupt
-void rx() { rxFlag = true; }
 
 void setup() {
   heltec_setup();
@@ -65,15 +66,13 @@ void setup() {
 
 void loop() {
   heltec_loop();
-
+  
   bool tx_legal = millis() > last_tx + minimum_pause;
   // Transmit a packet every PAUSE seconds or when the button is pressed
-  if ((PAUSE && tx_legal && millis() - last_tx > (PAUSE * 1000)) ||
-      button.isSingleClick()) {
+  if ((PAUSE && tx_legal && millis() - last_tx > (PAUSE * 1000)) || button.isSingleClick()) {
     // In case of button click, tell user to wait
     if (!tx_legal) {
-      both.printf("Legal limit, wait %i sec.\n",
-                  (int)((minimum_pause - (millis() - last_tx)) / 1000) + 1);
+      both.printf("Legal limit, wait %i sec.\n", (int)((minimum_pause - (millis() - last_tx)) / 1000) + 1);
       return;
     }
     both.printf("TX [%s] ", String(counter).c_str());
@@ -106,4 +105,9 @@ void loop() {
     }
     RADIOLIB_OR_HALT(radio.startReceive(RADIOLIB_SX126X_RX_TIMEOUT_INF));
   }
+}
+
+// Can't do Serial or display things here, takes too much time for the interrupt
+void rx() {
+  rxFlag = true;
 }
