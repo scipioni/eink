da0df32f097f045446424bb3d824ed2c357324f4
Stefano Scipioni <scipio.it@gmail.com>
2024-10-28 21:08:21 +0100

    fix: upload

--- micropython/led/main.py
+++ micropython/led/main.py
@@ -4,12 +4,11 @@ led_pin = 25  # helteck wifi lora 32 (v1)
 
 def blink():
     led = Pin(led_pin, Pin.OUT)
-    for i in range(10):
+    while True:
       led.on()
       time.sleep_ms(500)
       led.off()
       time.sleep_ms(500)
-      print("Blink ", i+1)
 
 if __name__ == "__main__":
     blink()
